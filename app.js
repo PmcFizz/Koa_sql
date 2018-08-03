171d46e9b303e5e57e5cf576ea528217af149bbc
pangmengchen <mengchen_0212@foxmail.com>
2018-08-03 16:37:18 +0800

    添加mysql支持

--- app.js
+++ app.js
@@ -8,12 +8,31 @@ const respond = require('koa-respond')
 const port = process.env.PORT || 3000
 const app = new Koa()
 const router = new Router()
+const mysql = require('mysql')
 app.use(Helmet())
 
 if (process.env.NODE_ENV === 'development') {
   app.use(Logger())
 }
 
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '123456',
+//   database: 'test'
+// })
+//
+// connection.connect()
+// let sql = 'SELECT * FROM websites'
+//
+// connection.query(sql, function (err, res) {
+//   if (!err) {
+//     console.log('************')
+//     console.log(res)
+//     console.log('************')
+//   }
+// })
+// connection.end()
 app.use(BodyParser({
   enableTypes: ['json'],
   jsonLimit: '2mb',
